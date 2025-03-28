05effd3daf588edec6522c7ec75b8687c18cbc93
mrrpiano02 <mrrpiano02@gmail.com>
2025-03-28 10:08:58 -0400

    fixed install shell script

--- install.sh
+++ install.sh
@@ -32,26 +32,20 @@ if [ $major -eq 3 ] && [ $minor -gt 11 ]; then
 		pip install virtualenv
 	fi
 
-	read -p "Python 3.11 will now be installed from source. sudo permissions are required for this. Type \"y\" to proceed. " permission
-	
-	if [ $permission == "y" ]; then
-		curl https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tgz > Python-3.11.0.tgz	
-		chmod +rwx Python-3.11.0.tgz
-		tar -xf Python-3.11.0.tgz
-
-		sudo sh -c Python-3.11.0/configure --enable-optimizations
-		sudo make -j $(nproc --all) Python-3.11.0
-		sudo make Python-3.11.0 altinstall
-
-		python -m virtualenv -p python3.11 venv
-		rm -r Python-3.11.0
-		rm Python-3.11.0.tgz
+	if [ -z `command -v python3.11` ]; then
+		echo "Python 3.11 is not installed. Install Python 3.11 using your system's package manager, or, alternatively, from source."
+		exit -1
 	fi
-	
-	echo "Installation complete. Please rerun the script under the new venv."
-	exit 0
 fi
 
+if [ -z `command -v python3.11` ]; then
+	python -m venv venv
+else
+	python3.11 -m venv venv
+fi
+
+source ./venv/bin/activate 
+ 
 pip install py7zr
 
 rm -f .tmp_pip_list
