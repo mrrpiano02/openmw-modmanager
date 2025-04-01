df247031535196f0f13d947c01f079f7f211ff37
mrrpiano02 <mrrpiano02@gmail.com>
2025-04-01 19:31:17 -0400

    took suggestions from shellcheck tool

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 #install script for Linux version of OpenMW mod manager
 
-if [ -z `command -v python` ]; then
+if [ -z $(command -v python) ]; then
 	echo "Your system does not have Python installed. Install Python through your system's package manager."
-	exit -1
+	exit 1
 fi
 
-if [ -z `command -v pip` ]; then
+if [ -z $(command -v pip) ]; then
 	echo "You must install pip before running this script. Install the python-pip package through your system's package manager."
-	exit -1
+	exit 1
 fi
 
 touch .tmp_pip_list
@@ -22,7 +22,7 @@ minor=$(echo $python_version | sed -e 's/[^.]*.//' -e 's/\..*//')
 
 if [ $major -lt 3 ] || [[ $major -eq 3 && $minor -lt 7 ]]; then
 	echo "Python must be at least version 3.7 to use this program."
-	exit -1
+	exit 1
 fi
 
 if [ $major -eq 3 ] && [ $minor -gt 11 ]; then
@@ -32,13 +32,13 @@ if [ $major -eq 3 ] && [ $minor -gt 11 ]; then
 		pip install virtualenv
 	fi
 
-	if [ -z `command -v python3.11` ]; then
+	if [ -z $(command -v python3.11) ]; then
 		echo "Python 3.11 is not installed. Install Python 3.11 using your system's package manager, or, alternatively, from source."
-		exit -1
+		exit 1
 	fi
 fi
 
-if [ -z `command -v python3.11` ]; then
+if [ -z $(command -v python3.11) ]; then
 	python -m venv venv
 else
 	python3.11 -m venv venv
